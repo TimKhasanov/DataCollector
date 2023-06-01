ba33dc52f054d983e55aef6a1521ef83acb2f6da
TimKhasanov <timm.hasanov95@gmail.com>
2023-06-01 16:07:38 +0300

    Vip

---
+++ src/main/java/models/Line.java
@@ -0,0 +1,7 @@
+package models;
+import lombok.Data;
+@Data
+public class Line {
+    private String name;
+    private String number;
+}
---
+++ src/main/java/models/Station.java
@@ -0,0 +1,11 @@
+package models;
+import lombok.Data;
+
+@Data
+public class Station {
+    private String name;
+    private String lineName;
+    private String date;
+    private String depth;
+    private boolean hasConnection;
+}
